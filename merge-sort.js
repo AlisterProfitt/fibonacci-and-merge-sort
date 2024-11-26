1b733af26ebc9392793e2459f27eac359727db3d
AlisterProfitt <alisterprofitt@gmail.com>
2024-11-26 12:46:59 -0400

    finish refactored merge sort

--- merge-sort.js
+++ merge-sort.js
@@ -25,4 +25,21 @@ function mergeSort(array) {
     return sortedArray;
 }
 
-console.log(mergeSort([3, 2, 1, 13]));
+function mergeSortRefactored(array) {
+    if (array.length === 0) return "Please Enter An Array With At Least 1 Item";
+    if (!Array.isArray(array)) return "Please Enter An Array Of Numbers";
+    if (array.length === 1) return array;
+    const left = mergeSort(array.slice(0, array.length / 2));
+    const right = mergeSort(array.slice(array.length / 2));
+    const sortedArray = [];
+    for (let i = 0, lIndex = 0, rIndex = 0; i < (left.length + right.length); i++) {
+        if (typeof left[lIndex] === 'undefined' || right[rIndex] <= left[lIndex]) {
+            sortedArray.push(right[rIndex++])
+        } else if (typeof right[rIndex] === 'undefined' || left[lIndex] <= right[rIndex]) {
+            sortedArray.push(left[lIndex++])
+        }
+    }
+    return sortedArray;
+}
+
+console.log(mergeSortRefactored([3, 2, 1, 3, 8, 5, 0, 1]));
